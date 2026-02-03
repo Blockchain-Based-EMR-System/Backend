88427ce1befaa3ac16cd1758268919cbdea50af3
enjyashraf18 <enjyashraf18@gmail.com>
2026-02-03 17:09:03 +0200

    fix: solve migration issues

--- src/prisma/migrations/20260202151555_add_off_time_parameters/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "DoctorSchedules" ADD COLUMN     "break_end" TEXT,
-ADD COLUMN     "break_start" TEXT,
-ADD COLUMN     "is_online" BOOLEAN NOT NULL DEFAULT true;
--- src/prisma/migrations/20260202193020_solve_timezone_problem/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "DoctorSchedules" ALTER COLUMN "start_time" SET DATA TYPE TEXT,
-ALTER COLUMN "end_time" SET DATA TYPE TEXT;
